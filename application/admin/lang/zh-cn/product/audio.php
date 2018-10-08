4628b9342866057e7ef415b83894df50ae2a5f3d
shengli <363912809@qq.com>
2018-10-08 20:23:43 +0800

    bug

--- application/admin/lang/zh-cn/product/audio.php
+++ application/admin/lang/zh-cn/product/audio.php
@@ -4,7 +4,7 @@ return [
     'Id'  =>  'ID',
     'Title'  =>  '标题',
     'Thumbimage'  =>  '缩略图',
-    'Audiofile'  =>  '音频',
+    'url'  =>  '跳转链接',
     'Band'  =>  '乐队',
     'Weigh'  =>  '权重',
     'Createtime'  =>  '创建时间'
