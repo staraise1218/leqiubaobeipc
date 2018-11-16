59f75dc5bdb9fbcfa65cc2f73cb8e4841a31bfcc
shengli <363912809@qq.com>
2018-11-16 09:59:55 +0800

    bug

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -4,6 +4,7 @@ namespace app\index\controller;
 
 use app\common\controller\Frontend;
 use app\common\library\Token;
+use think\Db;
 
 class Index extends Frontend
 {
@@ -19,6 +20,13 @@ class Index extends Frontend
 
     public function index()
     {
+        // 音频
+        $audiolist = Db::name('product_audio')
+            ->order('weigh desc, id desc')
+            ->limit(2)
+            ->select();
+
+        $this->assign('audiolist', $audiolist);
         return $this->view->fetch();
     }
 
