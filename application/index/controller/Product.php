b166e051892d7c221ae4f0278e7bfaa79e87df0c
shengli <363912809@qq.com>
2018-11-29 09:13:48 +0800

    zip

--- application/index/controller/Product.php
+++ application/index/controller/Product.php
@@ -83,4 +83,45 @@ class Product extends Frontend
         readfile($full_path);
         exit();
     }
+
+    public function downloadzip(){
+        $id = input('id');
+
+        $album = Db::name('product_album')->where('id', $id)->find();
+        $audio = Db::name('product_audio')
+            ->where('album_id', $id)
+            ->field('title, audiofile')
+            ->select();
+
+        $filename = './uploads/download/tmp'.'.zip';
+        $zip = new \ZipArchive();
+
+        if ($zip->open($filename, \ZIPARCHIVE::OVERWRITE || \ZIPARCHIVE::CREATE)!==TRUE) {
+            exit('无法打开文件，或者文件创建失败');
+        }
+        
+
+        if(is_array($audio) && !empty($audio)){
+            foreach($audio as $item){
+                $file = ltrim($item['audiofile'], '/');
+                if(!file_exists($file)){  
+                    exit("无法找到文件".$file); //即使创建，仍有可能失败。。。。  
+                }
+                if(file_exists($file)){
+                    $zip->addFile( $file, $item['title'].'.mp3');//第二个参数是放在压缩包中的文件名称，如果文件可能会有重复，就需要注意一下
+                }
+            }
+        }
+        $zip->close();//关闭
+        if(!file_exists($filename)){  
+            exit("无法找到文件"); //即使创建，仍有可能失败。。。。  
+        }
+        header("Cache-Control: public"); 
+        header("Content-Description: File Transfer"); 
+        header('Content-disposition: attachment; filename='.$album['title'].'.zip'); //文件名  
+        header("Content-Type: application/zip"); //zip格式的  
+        header("Content-Transfer-Encoding: binary"); //告诉浏览器，这是二进制文件  
+        header('Content-Length: '. filesize($filename)); //告诉浏览器，文件大小  
+        @readfile($filename);
+    }
 }
